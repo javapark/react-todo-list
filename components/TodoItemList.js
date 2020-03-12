6b852db42d7ef1fff5bf9d8d659e620ccfd07872
javapark <javaparknet@gmail.com>
2020-03-12 19:40:15 +0900

    세번째 컴포넌트

---
+++ components/TodoItemList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class TodoItemList extends Component{
+  render(){
+    const { todos, onToggle, onRemove} = this.props;
+
+    return (
+      <div></div>
+    );
+  };
+}
+
+
+export default TodoItemList;
+/*
+todos: todo 객체들이 들어있는 배열
+onToggle: 체크박스를 키고 끄는 함수
+onRemove: 아이템을 삭제시키는 함수
+*/
