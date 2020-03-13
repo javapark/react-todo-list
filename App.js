40799fdda45e63067b58f68d13176534ac6635f8
javapark <javaparknet@gmail.com>
2020-03-13 09:21:17 +0900

    state, props

--- App.js
+++ App.js
@@ -9,16 +9,54 @@ class App extends Component {
   state = {
     input: "",
     todos: [
-      { id: 0, text: "리액트 소개", checked: false },
-      { id: 1, text: "리액트 소개", checked: false },
-      { id: 2, text: "리액트 소개", checked: false }
+      { id: 0, text: "리액트 소개0", checked: false },
+      { id: 1, text: "리액트 소개1", checked: true },
+      { id: 2, text: "리액트 소개2", checked: false }
     ]
   };
 
+  handleChange = (e) => {
+    this.setState({
+      input : e.target.value
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: '',
+      todos : todos.concat({
+        id : this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 이면 handleCreate 호출
+    if(e.key === 'Enter'){
+      this.handleCreate();
+    }
+  }
+
   render() {
+    const {input, todos} = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this;
     return (
-      <TodoListTemplate form={<Form />}>
-        <TodoItemList />
+      <TodoListTemplate form={(
+        <Form 
+          value = {input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />
+      )}>
+        <TodoItemList todos={todos}/>
       </TodoListTemplate>
     );
   }
@@ -32,4 +70,8 @@ Form 기능
 - 텍스트 내용 바뀌면 state 업데이트
 - 버튼이 클릭되면 새로운 todo 생성 후 todos 업데이트
 - 인풋에서 Enter 누르면 버튼을 클릭한것과 동일한 작업진행하기
- */
+
+ 
+배열 사용시 push 를 하면 안됨
+ 
+*/
--- components/TodoItemList.js
+++ components/TodoItemList.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem';
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
 
-class TodoItemList extends Component{
-  render(){
-    const { todos, onToggle, onRemove} = this.props;
+class TodoItemList extends Component {
+  render() {
+    const { todos, onToggle, onRemove } = this.props;
 
-    return (
-      <div>
-        <TodoItem text="안녕"/>
-        <TodoItem text="React" />
-      </div> 
-   );
-  };
+    const todoList = todos.map(
+      // ({id, text, checked}) => (
+      //   <TodoItem
+      //     id={id}
+      //     text={text}
+      //     checked={checked}
+      //     onToggle={onToggle}
+      //     onRemove={onRemove}
+      //     key={id}
+      //   />
+      // )
+
+      (todo) => (
+        <TodoItem {...todo} 
+          onToggle={onToggle} 
+          onRemove={onRemove} 
+          key={todo.id} 
+        />
+      )
+    );
+
+    return <div>{todoList}</div>;
+  }
 }
-
 
 export default TodoItemList;
 /*
